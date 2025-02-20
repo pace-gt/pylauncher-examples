4d583296d6b3a80a8aa7bf7bdac6c637d2ea7ff9
Ron Rahaman <rrahaman6@gatech.edu>
2025-02-20 14:04:05 -0500

    RR: Cleanup mpi_hello.c

--- 04_mpi/mpi_hello.c
+++ 04_mpi/mpi_hello.c
@@ -1,33 +1,32 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "unistd.h"
 #include "mpi.h"
+#include "unistd.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc,char **argv) {
-  int jobno,slp,mytid,ntids;
-  char outfile[5+5+5+1];
-  FILE *f;
+int main(int argc, char *argv[]) {
+  int job_id, sleep_sec, my_rank, n_ranks;
 
-  MPI_Init(&argc,&argv);
-  MPI_Comm_size(MPI_COMM_WORLD,&ntids);
-  MPI_Comm_rank(MPI_COMM_WORLD,&mytid);
-  if (argc<2) {
-    if (mytid==0) printf("Usage: parallel id slp\n");
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &n_ranks);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+  if (my_rank == 0 && argc < 2) {
+    printf("Usage: mpi_hello job_id sleep_seconds\n");
+    MPI_Abort(MPI_COMM_WORLD, 1);
   }
-  jobno = atoi(argv[1]);
-  slp = atoi(argv[2]);
 
-  MPI_Barrier(MPI_COMM_WORLD);
-  sprintf(outfile,"pytmp-%04d-%04d",jobno,mytid);
-  f = fopen(outfile,"w");
-  fprintf(f,"%d/%d working\n",mytid,ntids);
+  job_id = atoi(argv[1]);
+  sleep_sec = atoi(argv[2]);
+
+  char outfile[16];
+  sprintf(outfile, "pytmp-%04d-%04d", job_id, my_rank);
+
+  FILE *f = fopen(outfile, "w");
+  fprintf(f, "%d/%d working\n", my_rank, n_ranks);
   fclose(f);
 
-  if (mytid==0) {
-    printf("Job %d on %d processors\n",jobno,ntids);
+  if (my_rank == 0) {
+    printf("Job %d on %d processors\n", job_id, n_ranks);
   }
-  sleep(slp);
+  sleep(sleep_sec);
   MPI_Finalize();
-  return 0;
 }
-
