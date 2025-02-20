eefcbd475bf5e111251656b232e511fa2d420dbf
Ron Rahaman <rrahaman6@gatech.edu>
2025-02-20 13:33:29 -0500

    RR: Cleanup constant_sn

---
+++ 02_constant_sn/constant_sn.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+from pylauncher import pylauncher
+
+pylauncher.ClassicLauncher("constant_sn.in", debug="", cores=6)
--- constant-sn/constant_sn_launcher.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-from pylauncher import pylauncher
-
-myjob = "constant_sn_job.in"
-pylauncher.ClassicLauncher(myjob, debug="", cores=3)
