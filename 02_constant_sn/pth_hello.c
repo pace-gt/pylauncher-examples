a6b2672167694b1ed66a85867865fbd4070649fa
Ron Rahaman <rrahaman6@gatech.edu>
2025-02-20 14:53:03 -0500

    RR: constant_sn and variable_sn give predictable results

--- 02_constant_sn/pth_hello.c
+++ 02_constant_sn/pth_hello.c
@@ -1,40 +1,41 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+#include <unistd.h>
 
 int thread_count;
 
-void* Hello(void* rank);
+void *hello(void *rank);
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
   long thread;
-  pthread_t* thread_handles;
+  pthread_t *thread_handles;
 
   thread_count = strtol(argv[1], NULL, 10);
 
-  thread_handles =  malloc ( thread_count*sizeof(pthread_t));
+  thread_handles = malloc(thread_count * sizeof(pthread_t));
 
-  for (thread=0; thread < thread_count; thread++)
-    pthread_create(&thread_handles[thread], NULL, Hello, (void*) thread);
+  for (thread = 0; thread < thread_count; thread++)
+    pthread_create(&thread_handles[thread], NULL, hello, (void *)thread);
 
-  printf("Hello from the main thread\n");
+  printf("hello from the main thread\n");
 
-  for (thread=0; thread<thread_count; thread++)
+  for (thread = 0; thread < thread_count; thread++)
     pthread_join(thread_handles[thread], NULL);
 
   free(thread_handles);
   return 0;
-
 }
 
-void* Hello(void* rank)
-{
-  long my_rank = (long) rank;
+void *hello(void *rank) {
+  long my_rank = (long)rank;
 
-  printf("Hello from thread %ld of %d\n", my_rank, thread_count);
+  printf("hello from thread %ld of %d\n", my_rank, thread_count);
+  long j;
+  for (j = 0; j < 1000000000; ++j) {
+    continue;
+  }
+  printf("%ld\n", j);
 
   return NULL;
 }
-
-
